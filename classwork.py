47807cab450e0ee3d0e72885508bd5a1da3b0ed2
tomidevops <majortomiodukoya@gmail.com>
2022-09-28 15:44:29 +0100

    maths_classwork

---
+++ classwork.py
@@ -0,0 +1,12 @@
+# Find x if 4 = 2x+5
+x = (4-5)/2
+print(x)
+
+# Find the area of a triangle using heros formula if a=5, b=7, c=10
+a = 5
+b = 7
+c = 10
+s = (a+b+c)//2
+print(s)
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+print(area)
