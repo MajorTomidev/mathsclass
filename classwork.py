3f2fc053f4b04b32b739632166e777b4524fb843
tomidevops <majortomiodukoya@gmail.com>
2022-10-05 14:26:49 +0100

    updateonclasswork

--- classwork.py
+++ classwork.py
@@ -16,3 +16,42 @@ km = 10
 t_miles = km / miles
 print (t_miles)
 
+# if you run a 10km race in 42mins 42secs, what is your average pace (time per mile in minutes and seconds)? what is your average speed in miles per hour
+
+distance = 10
+
+# time 
+tim_mins = 42
+tim_secs = 42
+
+# Minutes
+minutes = tim_mins + (tim_secs/60)
+print (minutes)
+
+# Seconds
+seconds = (tim_secs*60) + 42
+print (seconds)
+
+# Hour
+hour = minutes/60
+print(hour)
+
+# Miles per Minutes
+miles_mins = t_miles / minutes
+print (miles_mins)
+
+# Miles per Seconds
+miles_secs = t_miles / seconds
+print (miles_secs)
+
+# Miles per Hour
+miles_hour = t_miles / hour
+print (miles_hour)
+
+pie = 3.142
+r = 5 
+volume = 4/3 * pie * r**3
+
+print (volume)
+
+
